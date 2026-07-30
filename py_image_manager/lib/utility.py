2e45399445c3528c85364f07d189f00e8af31d42
Giovanni Flore <flore.giovanni.scripting@outlook.com>
2026-07-30 17:56:31 +0200

    todo

--- py_image_manager/lib/utility.py
+++ py_image_manager/lib/utility.py
@@ -5,3 +5,6 @@ class Image:
         self.location = path
         self.size = path.stat().st_size
         pass
+
+# what data do I want to extract from exif?
+# define accepted formats (research what pillow/python supports)
